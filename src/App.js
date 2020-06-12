505723cf3de2aed69de9118e6b8f4e0d8870a714
Anuj Goyal <goyal.anuj973@gmail.com>
2020-06-12 17:31:24 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 //import { Navbar, NavbarBrand } from 'reactstrap';
 import Main from './components/MainComponent';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 //import { DISHES } from './shared/dishes';
 
 class App extends Component {
@@ -15,9 +16,11 @@ class App extends Component {
   }*/
   render() {
     return (
-      <div>
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
