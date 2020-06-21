b8a9735414086f9d4dd2ebc5b8f7c794dfc53269
Anuj Goyal <goyal.anuj973@gmail.com>
2020-06-21 17:21:33 +0530

    Fetch Handling Errors

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
      /*constructor(props) {
         super(props);
         
@@ -17,8 +18,8 @@ import { Loading } from './LoadingComponent';
     function RenderMenuItem({dish, onClick}) {
         return (
             <Card> 
-                <Link to={'/menu/${dish.id}'} >
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />                    
+                <Link to={`/menu/${dish.id}`} >
+                    <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />                    
                         <CardImgOverlay>
                             <CardTitle> {dish.name} </CardTitle>
                         </CardImgOverlay>  
